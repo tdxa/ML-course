8a9ff02fc61d30f0b6daad6497b658a2a5da522b
tdxa <anndoman3@gmail.com>
2022-05-09 03:34:55 +0200

    Draw decision tree

--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
-import csv, collections
+import collections
+import csv
 from math import log2
 
 FILE = './data/gielda.txt'
 
-def read_file(path:str, delimiter=","):
+def read_file(path: str, delimiter=","):
     with open(path) as file:
         return [row for row in csv.reader(file, delimiter=delimiter)]
+
 
 def count_class(data):
     size = len(data[0])
@@ -15,13 +17,15 @@ def count_class(data):
         counts.append(len(uniques))
     return counts
 
+
 def count_occurences(data):
     occurences = []
-    for column in zip(*data): # zip transposes rows to columns
+    for column in zip(*data):  # zip transposes rows to columns
         occurences.append(dict(collections.Counter(item for item in column)))
     return occurences
 
-def probabilities(data):
+
+def count_probabilities(data):
     probs = []
     for column in data:
         total = sum(column.values())
@@ -32,13 +36,15 @@ def probabilities(data):
 def count_entropy(decision_probabilities):
     return -(sum([p * log2(p) for p in decision_probabilities if p != 0]))
 
-def entropy(probabilities):
+
+def get_entropy(probabilities):
     """
     Wyliczanie entropii dla klasy decyzyjnej na podstawie prawdopodbieństw
     """
     return count_entropy(probabilities[-1])
 
-def info(attribute_index, data):
+
+def get_info(attribute_index, data):
     """
     Wyliczanie funkcji informacji dla atrybutu warunkowego
     """
@@ -46,51 +52,70 @@ def info(attribute_index, data):
     info = 0
     for attribute in occurences[attribute_index].keys():
         subset = [row for row in data if row[attribute_index] == attribute]
-        subset_probabilities = probabilities(count_occurences(subset))
-        info += (len(subset) / len(data) * entropy(subset_probabilities))
+        subset_probabilities = count_probabilities(count_occurences(subset))
+        info += (len(subset) / len(data) * get_entropy(subset_probabilities))
     return info
 
-def gain(entropy, info):
-    return [(entropy-attribute) for attribute in info]
 
-def split_info(attribute_index, probabilities):
+def get_gain(entropy, info):
+    return [(entropy - attribute) for attribute in info]
+
+
+def get_split_info(attribute_index, probabilities):
     """
     Wyliczanie informacji o podziale tj. liczenie entropii dla atrybutu warunkowego
     """
     return count_entropy(probabilities[attribute_index])
 
-def gain_ratio(gain, split_info):
-    return [(gain[attribute] / split_info[attribute]) for attribute in range(len(gain))]
 
-def best_attribute(gain_ratio):
+def get_gain_ratio(gain, split_info):
+    return [(gain[attribute] / split_info[attribute]) if split_info[attribute] > 0 else 0 for attribute in
+            range(len(gain))]
+
+
+def get_best_attribute(gain_ratio):
     """
     Wybranie atrybutu warunkowego według którego nastąpi podział w drzewie decyzyjnym
     """
-    return gain_ratio.index(max(gainratio))
+    return gain_ratio.index(max(gain_ratio))
+
+
+def build_tree(data, prev=-1, margin='\t\t'):
+    # print("DATA   ", data)
+    classes = count_class(data)
+    # print("CLASSES   ", classes)
+    occurences = count_occurences(data)
+    # print("OCCURENCES   ", occurences)
+    probabilities = count_probabilities(occurences)
+    # print("PROBABILITIES   ", probabilities)
+    entropy = get_entropy(probabilities)
+    # print("ENTROPY   ", entropy)
+    info = [get_info(index, data) for index in range(len(classes) - 1)]
+    # print("INFO   ", info)
+    gain = get_gain(entropy, info)
+    # print("GAIN   ", gain)
+    split_info = [get_split_info(index, probabilities) for index in range(len(classes) - 1)]
+    gain_ratio = get_gain_ratio(gain, split_info)
+
+    max_gain_ratio = max(gain_ratio)
+    best_attribute = get_best_attribute(gain_ratio)
+
+    if max_gain_ratio > 0:  # warunek stopu
+        if prev != -1:
+            print(f'{margin}{list(occurences[prev].keys())[0]} --> ATTRIBUTE: {best_attribute + 1}')
+        else:
+            print(f'ATTRIBUTE: {best_attribute + 1}')
+
+        prev = best_attribute
+        margin += '\t'
+        new = [[x for x in data if x[best_attribute] == key] for key in occurences[best_attribute]]
+
+        for x in new:
+            build_tree(x, prev, margin)
+    else:
+        print(f'{margin}{list(occurences[prev].keys())[0]} --> DECISION: {list(occurences[len(data[0]) - 1].keys())[0]}')
+
 
 x = read_file(FILE)
-clss = count_class(x)
-occ= count_occurences(x)
-prop = probabilities(count_occurences(x))
-entr = entropy(prop)
-info = [info(index, x) for index in range(len(clss) - 1)]
-gai = gain(entr, info)
-split = [split_info(index, prop) for index in range(len(clss) - 1)]
-gainratio = gain_ratio(gai,split)
-best = best_attribute(gainratio)
-
-print("*************************")
-print(x)
-print(list(zip(*x)))
-print(clss)
-print(occ)
-print(prop)
-print(entr)
-print(info)
-print(gai)
-print(split)
-print(gainratio)
-print(best)
-
-
+build_tree(x)
 
