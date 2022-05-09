6461fd42241dbc7e0957d9d2c3b03a10e3588789
tdxa <anndoman3@gmail.com>
2022-05-09 02:35:58 +0200

    Create Gain function

--- utils.py
+++ utils.py
@@ -23,42 +23,63 @@ def count_occurences(data):
         occurences.append(dict(collections.Counter(item for item in column)))
     return occurences
 
-def get_probabilities(data):
-    probabilities = []
+def probabilities(data):
+    probs = []
     for column in data:
         total = sum(column.values())
-        probabilities.append([attribute/total for attribute in list(column.values())])
-    return probabilities
+        probs.append([attribute / total for attribute in list(column.values())])
+    return probs
 
 
 def count_entropy(decision_probabilities):
     return -(sum([p * log2(p) for p in decision_probabilities if p != 0]))
 
-def get_entropy(probabilities):
+def entropy(probabilities):
     """
     Wyliczanie entropii dla klasy decyzyjnej na podstawie prawdopodbieństw
     """
     return count_entropy(probabilities[-1])
 
-def get_info(attribute_class, data):
+def info(attribute_index, data):
     """
-    Wyliczanie funkcji informacji dla podanej klasy atrybutu warunkowego
+    Wyliczanie funkcji informacji dla atrybutu warunkowego
     """
     occurences = count_occurences(data)
     info = 0
-    for attribute in occurences[attribute_class].keys():
-        subset = [row for row in x if row[attribute_class] == attribute]
-        subset_probabilities = get_probabilities(count_occurences(subset))
-        info += (len(subset) / len(x) * get_entropy(subset_probabilities))
+    for attribute in occurences[attribute_index].keys():
+        subset = [row for row in data if row[attribute_index] == attribute]
+        subset_probabilities = probabilities(count_occurences(subset))
+        info += (len(subset) / len(data) * entropy(subset_probabilities))
     return info
 
+def gain(entropy, info):
+    return [(entropy-attribute) for attribute in info]
+
+def split_info(attribute_index, probabilities):
+    """
+    Wyliczanie informacji o podziale tj. liczenie entropii dla atrybutu warunkowego
+    """
+    return count_entropy(probabilities[attribute_index])
+
+def gain_ratio(gain, split_info):
+    return [(gain[attribute] / split_info[attribute]) for attribute in range(len(gain))]
+
+def best_attribute(gain_ratio):
+    """
+    Wybranie atrybutu warunkowego według którego nastąpi podział w drzewie decyzyjnym
+    """
+    return gain_ratio.index(max(gainratio))
 
 x = read_file(FILE)
 clss = count_class(x)
 occ= count_occurences(x)
-prop = get_probabilities(count_occurences(x))
-entr = get_entropy(prop)
-info = [get_info(index, x) for index in range(len(clss)-1)]
+prop = probabilities(count_occurences(x))
+entr = entropy(prop)
+info = [info(index, x) for index in range(len(clss) - 1)]
+gai = gain(entr, info)
+split = [split_info(index, prop) for index in range(len(clss) - 1)]
+gainratio = gain_ratio(gai,split)
+best = best_attribute(gainratio)
 
 print("*************************")
 print(x)
@@ -68,5 +89,10 @@ print(occ)
 print(prop)
 print(entr)
 print(info)
-print()
+print(gai)
+print(split)
+print(gainratio)
+print(best)
+
+
 
